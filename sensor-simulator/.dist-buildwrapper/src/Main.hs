7376f80605d2d8e925c8b7d1cc2529df04032e77
Max Khardin <max.khardin@gmail.com>
2013-06-02 17:40:42 +0400

    Some initial functions and datatypes

---
+++ sensor-simulator/.dist-buildwrapper/src/Main.hs
@@ -0,0 +1,28 @@
+module Main where
+import qualified Data.Vector.V2 as V
+
+
+data Vec2D = Vec2D Double Double deriving Show
+newtype Position = Pos Vec2D
+newtype Rotation = Rot Vec2D
+
+
+normalizedVector :: Double -> Double -> Vec2D
+normalizedVector x y = Vec2D (x/norm) (y/norm)
+                        where norm = sqrt $ x*x + y*y
+
+data RobotState = RobotState Position Rotation
+main::IO()
+main = undefined
+
+rotate :: RobotState -> Rotation -> RobotState
+rotate st rot = undefined
+
+rotateVectorByAngle :: Vec2D -> Double -> Vec2D
+rotateVectorByAngle (Vec2D x y) θ = Vec2D ((x * cos θ) - (y * sin θ)) ((x * sin θ) + (y * cos θ))
+
+moveForward :: RobotState -> Double -> RobotState
+moveForward (RobotState pos rot) len = undefined
+
+scalar
+
