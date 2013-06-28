8303c5b0333933064634f1b130508ab384479ebe
Max Khardin <max.khardin@gmail.com>
2013-06-28 22:49:20 +0400

    First working version, read from stdin, noiseless moving and rotation

--- src/Main.hs
+++ src/Main.hs
@@ -2,17 +2,33 @@ module Main where
 import qualified Data.Vector.V2 as V
 import qualified Data.Vector.Transform.T2 as T
 import Data.Vector.Class
-
+import Control.Monad
 
 type Position = V.Vector2
 type Rotation = V.Vector2
 
-data RobotState = RobotState Position Rotation
-main::IO()
-main = undefined
+data RobotState = RobotState Position Rotation 
+	deriving Show
 
-rotate :: RobotState -> Rotation -> RobotState
-rotate st rot = undefined
+
+main::IO()
+main = do
+	let initialState = RobotState (V.Vector2 0 0) (V.Vector2 1 0)
+	readAndUpdateState initialState
+	return ()
+
+
+readAndUpdateState currentState = do
+								θ <- getLine
+								l <- getLine 
+								let newState = rotateAndMove (read θ ::Double) (read l ::Double) currentState
+								putStrLn $ show newState
+								readAndUpdateState newState
+
+rotateAndMove :: Scalar -> Scalar -> RobotState -> RobotState
+rotateAndMove θ l (RobotState pos rot) = moveForward (RobotState pos newrot) l
+									where newrot = rotateVectorByAngle rot θ
+
 
 rotationTransform :: Scalar -> T.Transform2
 rotationTransform θ = T.Transform2 (cos θ) (- sin θ) 0
@@ -25,3 +41,8 @@ rotateVectorByAngle v θ = T.transformP2 (rotationTransform θ) v
 
 moveForward :: RobotState -> Double -> RobotState
 moveForward (RobotState pos rot) len = RobotState (len *| rot + pos) rot
+
+
+rotate :: RobotState -> Double -> RobotState
+rotate (RobotState pos rot) θ = RobotState pos (rotateVectorByAngle rot θ)
+
