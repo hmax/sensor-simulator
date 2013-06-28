6e1b7a1df588aaec18f69338256d52fb02025b9b
Max Khardin <max.khardin@gmail.com>
2013-06-28 21:41:36 +0400

    Migrate to AC-Vector instead of custom vectors

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,11 @@
 module Main where
 import qualified Data.Vector.V2 as V
+import qualified Data.Vector.Transform.T2 as T
+import Data.Vector.Class
 
 
-data Vec2D = Vec2D Double Double deriving Show
-newtype Position = Pos Vec2D
-newtype Rotation = Rot Vec2D
-
-
-normalizedVector :: Double -> Double -> Vec2D
-normalizedVector x y = Vec2D (x/norm) (y/norm)
-                        where norm = sqrt $ x*x + y*y
+type Position = V.Vector2
+type Rotation = V.Vector2
 
 data RobotState = RobotState Position Rotation
 main::IO()
@@ -18,8 +14,14 @@ main = undefined
 rotate :: RobotState -> Rotation -> RobotState
 rotate st rot = undefined
 
-rotateVectorByAngle :: Vec2D -> Double -> Vec2D
-rotateVectorByAngle (Vec2D x y) θ = Vec2D ((x * cos θ) - (y * sin θ)) ((x * sin θ) + (y * cos θ))
+rotationTransform :: Scalar -> T.Transform2
+rotationTransform θ = T.Transform2 (cos θ) (- sin θ) 0
+                                   (sin θ) (cos θ)   0
+
+
+rotateVectorByAngle :: V.Vector2 -> Double -> V.Vector2
+rotateVectorByAngle v θ = T.transformP2 (rotationTransform θ) v
+
 
 moveForward :: RobotState -> Double -> RobotState
-moveForward (RobotState pos rot) len = undefined
+moveForward (RobotState pos rot) len = RobotState (len *| rot + pos) rot
